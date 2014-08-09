2a9a0333135f123af4d84c242ca61fcda5689822
Rafael <ti.rcampanari@gmail.com>
2014-08-09 02:58:02 -0300

    Mobile menu added

--- admin/template_head.php
+++ admin/template_head.php
@@ -10,31 +10,77 @@
 
   $this->printMessage(); 
   ?>
+
+  <div class="menuLeftMobile animated">
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <i style='font-size:25px; color:white; cursor:pointer;' class='fa fa-arrow-circle-o-left pull-right' onclick="$('.menuLeftMobile').css('left','-300px');"></i>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <h4 style="color:white;">Menu</h4>
+      </div>
+    </div>
+    <?
+      if ($this->menu_left) {
+          foreach ($this->menu_left  as $key => $item_menu){
+            if ( $item_menu['fkaccess_tool'] == null || $_SESSION['admin']['access'][$item_menu['code']] ) {
+              ?>
+                <div>
+                  <a href="<?php echo $item_menu['link']?>" title="<?php echo $item_menu['mask']?>" >
+                    <i class="fa fa-<?php echo ($item_menu['icon']!="")? $item_menu['icon'] : "paperclip" ?> fa-fw"></i> <?php echo $item_menu['mask']?>
+                  </a>
+                </div>
+              <?php 
+            } 
+          }
+        }
+  ?>
+  <?php   
+    if ($_SESSION['admin']['access']['_use']) {?>
+      <a href="./?l=user" title="Configurações">
+        <i class="fa fa-users fa-fw"></i> Usuários
+      </a>
+    <?php  }
+    ?>
+    <?php   
+    if ($_SESSION['admin']['access']['_his']) {?>
+      <a href="./?l=history" title="Configurações">
+        <i class="fa fa-bar-chart-o fa-fw"></i> Histórico
+      </a>
+    <?php  }
+    ?>
+  </div>
+
   <div class="container-fluid">
    <div class="row">
      <div class="col-sm-12">
      </div>
    </div>
    <div class="row">
-    <div class="col-sm-3 col-md-2 sidebar  text-center">
-      <div >
-      <h3>
-        Project
-      </h3>
-        <small id="logout">      Olá <?php echo $_SESSION['admin']['user']['login']?>, seu último acesso foi <?php echo $this->Util->getCurrentDate('d/m/Y \a\s h:i');?> (<a href="./login/logout">sair</a>)
-        </small>
-        <div id="menuToggle">
-          <div></div>
-          <div></div>
-          <div></div>
-        </div> 
-        <nav id="menu" class='row' >
-          <ul>
-            <?php  
+    <div class="col-sm-3 col-md-2  text-center">
+    <div class='sidebar'>
+        <div >
+          <h3>
+            Project
+          </h3>
+          <small id="logout">      Olá <?php echo $_SESSION['admin']['user']['login']?>, seu último acesso foi <?php echo $this->Util->getCurrentDate('d/m/Y \a\s h:i');?> (<a href="./login/logout">sair</a>)
+          </small>
+          <div id="menuToggle" onclick="$('.menuLeftMobile').css('left',0); ">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div> 
+          <nav id="menu" class='row' >
+            <ul>
+              <?php  
               require ADMIN.'core/view/menu_left.php'; 
-            ?>
-          </ul>
-        </nav>
-      </div> 
+              ?>
+            </ul>
+          </nav>
+        </div> 
+
+      </div>
     </div>
     <div id="content" class="col-sm-9 col-md-10">
