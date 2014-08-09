4cf5cba30303f557e775bb976a1a040d72de9369
Rafael <ti.rcampanari@gmail.com>
2014-08-09 16:06:48 -0300

    Directory tree adjusts - Dynamic Site Title

--- admin/login/view/home.php
+++ admin/login/view/home.php
@@ -7,11 +7,11 @@
 	<div class="login-container">
 		<div id="output">
 		</div>
-		<div class="avatar">
-			<i class='fa fa-user'></i>
+		<div class="avatar ">
+			<span class='glyphicon glyphicon-user ' style='color:#eee; text-shadow: 0 0 1px #ccc; font-size:40px; margin-top:25px;'></span>
 		</div>
 		<div>
-			<h4>Control Panel</h4>
+			<h4>Painel de Controle</h4>
 		</div>
 		<div class="form-box">
 			<form  role="form" action="./login/auth" method="post"   accept-charset="utf-8">
